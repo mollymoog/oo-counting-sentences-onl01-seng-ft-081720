3a2e688780f281fa3cb89ef0967c23610f071ba2
molly mk <mmkover@gmail.com>
2020-08-28 03:12:19 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,8 +15,7 @@ class String
   end
 
   def count_sentences
-    self.split("."||"?"||"!")
-    self.delete_if {|word| word.size < 2}
+    self.split("."||"?"||"!").delete_if {|word| word.size < 2}
     binding.pry
      
   end
